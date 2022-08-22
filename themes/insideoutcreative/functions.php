dbb31d5c7eac98f3c86d80c6ca5e87d8fb0d16dd
Efrain Lemus <efraintlemusm@gmail.com>
2022-08-22 15:19:41 -0400

    update function name and remove action for wp_block_library

--- themes/insideoutcreative/functions.php
+++ themes/insideoutcreative/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function done_right_signs_stylesheets() {
+function peerless_industries_stylesheets() {
 wp_enqueue_style('style', get_stylesheet_uri() );
 
 wp_enqueue_style('bootstrap', get_theme_file_uri('/css/bootstrap.min.css'));
@@ -38,9 +38,9 @@ wp_enqueue_style('blair-itc', get_theme_file_uri('/blair-itc/blair-itc.css'));
 // wp_enqueue_style('font-nexa', get_theme_file_uri('/font-nexa/font-nexa.css'));
 
 }
-add_action('wp_enqueue_scripts', 'done_right_signs_stylesheets');
+add_action('wp_enqueue_scripts', 'peerless_industries_stylesheets');
 // for footer
-function done_right_signs_stylesheets_footer() {
+function peerless_industries_stylesheets_footer() {
 	// wp_enqueue_style('style-footer', get_theme_file_uri('/css/style-footer.css'));
 	// owl carousel
 	wp_enqueue_style('owl.carousel.min', get_theme_file_uri('/owl-carousel/owl.carousel.min.css'));
@@ -76,7 +76,7 @@ function done_right_signs_stylesheets_footer() {
 		}
 	}
 	
-add_action('get_footer', 'done_right_signs_stylesheets_footer');
+add_action('get_footer', 'peerless_industries_stylesheets_footer');
 
 // loads enqueued javascript files deferred
 function mind_defer_scripts( $tag, $handle, $src ) {
@@ -100,7 +100,7 @@ function mind_defer_scripts( $tag, $handle, $src ) {
   } 
   add_filter( 'script_loader_tag', 'mind_defer_scripts', 10, 3 );
 
-function done_right_signs_menus() {
+function peerless_industries_menus() {
  register_nav_menus( array(
    'primary' => __( 'Primary' )));
 register_nav_menus( array(
@@ -110,7 +110,7 @@ register_nav_menus( array(
  add_theme_support('post-thumbnails');
 }
 
-add_action('after_setup_theme', 'done_right_signs_menus');
+add_action('after_setup_theme', 'peerless_industries_menus');
 
 if( function_exists('acf_add_options_page') ) {
 
@@ -126,7 +126,7 @@ function smartwp_remove_wp_block_library_css(){
 		wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
 	}
 } 
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+// add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 
 add_filter('show_admin_bar', '__return_false');
 
